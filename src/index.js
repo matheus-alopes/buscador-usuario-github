33b1ef535ed7575a64e224c23f5cb0960f7d2eaa
Matheus Alencar <matlopes96@outlook.com>
2023-01-07 17:32:13 -0300

    adicionei os repositorios na busca

--- src/index.js
+++ src/index.js
@@ -9,17 +9,50 @@ async function user(userNick) {
     return responseJson;
 }
 
+async function repos(userNick) {
+    const response = await fetch(`https://api.github.com/users/${userNick}/repos`);
+
+    const responseJson = await response.json();
+
+    return responseJson;
+}
+
 function getUserProfile(userNick) {
     user(userNick).then(
         userData => {
-            let userInfo = `<img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
-                            <div class="data">
-                                <h1>${userData.name ?? "Não possui nome cadastrado 😢"}</h1>
-                                <p>${userData.bio ?? "Não possui bio cadastrada 😢"}</p>
+            let userInfo = 
+            `<div class = "info">
+                <img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
+                <div class="data">
+                    <h1>${userData.name ?? "Não possui nome cadastrado 😢"}</h1>
+                    <p>${userData.bio ?? "Não possui bio cadastrada 😢"}</p>
                                 
-                            </div>`
+                </div>
+            </div>`
 
             document.querySelector(".profile-data").innerHTML = userInfo;
+        }
+    )
+}
+
+function getUserRepositories(userNick) {
+    repos(userNick).then(
+        reposData => {
+            let repositoriesItens = "";
+
+            reposData.forEach(
+                repo => {
+                    repositoriesItens += `<li>
+                                        <a href = "${repo.html_url}" target="_blank">${repo.name}</a>
+                                        </li>`
+                }
+            )
+
+            document.querySelector(".profile-data").innerHTML += 
+            `<div class = "repositories section">
+                <h2>Repositórios</h2>
+                <ul>${repositoriesItens}</ul>
+            </div>`
         }
     )
 }
@@ -27,15 +60,19 @@ function getUserProfile(userNick) {
 searchButton.addEventListener("click", 
     () => {
         getUserProfile(userNameInput.value);
+        getUserRepositories(userNameInput.value);
     }
 )
 
 userNameInput.addEventListener("keyup", 
     (e) => {
         let key = e.which || e.keyCode;
+
         const isEnterKeyPressed = key === 13;
+
         if (isEnterKeyPressed) {
             getUserProfile(userNameInput.value);
+            getUserRepositories(userNameInput.value);
         }
     }
 )
